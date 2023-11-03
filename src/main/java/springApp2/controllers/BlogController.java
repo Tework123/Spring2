a6985a71ffccf98dcc75dce8e1ed4a3b30801381
Tework123 <daniliesaec@gmail.com>
2023-11-03 16:56:14 +0300

    spring boot day1

--- src/main/java/springApp2/controllers/BlogController.java
+++ src/main/java/springApp2/controllers/BlogController.java
@@ -1,40 +1,40 @@
 package springApp2.controllers;
 
-import org.hibernate.Session;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import springApp2.config.HibernateUtil;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import springApp2.models.Blog;
+import springApp2.services.BlogService;
 
 
 @Controller
 public class BlogController {
-    @Value("${db}")
-    private String db;
+    private final BlogService blogService;
 
-    @GetMapping()
-    public String index() {
+    public BlogController(BlogService blogService) {
+        this.blogService = blogService;
+    }
 
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        try {
-            session.beginTransaction();
-
-            Blog blog = session.get(Blog.class, 5);
-            System.out.println(blog);
-
-
-            session.getTransaction().commit();
-            session.close();
-
-        } finally {
-            HibernateUtil.shutdown();
-
-        }
-
-
-        System.out.println(db);
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("blogs", blogService.listBlogs());
+        System.out.println(blogService.listBlogs());
+        System.out.println("asd");
         return "blog/index";
+
+    }
+
+    @GetMapping("/create")
+    public String createBlog0(@ModelAttribute("blog") Blog blog) {
+        return "blog/new";
+    }
+
+    @PostMapping("/create")
+    public String createBlog(Blog blog) {
+        blogService.saveBlog(blog);
+        return "redirect:/";
 
     }
 }
--- src/main/java/springApp2/models/Blog.java
+++ src/main/java/springApp2/models/Blog.java
@@ -1,45 +1,48 @@
 package springApp2.models;
 
 import java.io.Serializable;
-
-import jakarta.persistence.*;
+//
+//import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-@Entity
-@Table(name = "blog", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "id"),
-        @UniqueConstraint(columnNames = "email")})
+//
+//@Entity
+//@Table(name = "blog", uniqueConstraints = {
+//        @UniqueConstraint(columnNames = "id"),
+//        @UniqueConstraint(columnNames = "email")})
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Blog implements Serializable {
+    private String title;
+    private int age;
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name = "id", unique = true, nullable = false)
-    private Integer id;
-
-
-    @Column(name = "email", unique = true, nullable = false, length = 100)
-    private String email;
-
-    @Column(name = "firstname", unique = false, nullable = false, length = 100)
-    private String firstname;
-
-    @Column(name = "lastname", unique = false, nullable = false, length = 100)
-    private String lastname;
-
-    @Override
-    public String toString() {
-        return "Blog{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                '}';
-    }
+//
+//    @Id
+//    @GeneratedValue(strategy=GenerationType.IDENTITY)
+//    @Column(name = "id", unique = true, nullable = false)
+//    private Integer id;
+//
+//
+//    @Column(name = "email", unique = true, nullable = false, length = 100)
+//    private String email;
+//
+//    @Column(name = "firstname", unique = false, nullable = false, length = 100)
+//    private String firstname;
+//
+//    @Column(name = "lastname", unique = false, nullable = false, length = 100)
+//    private String lastname;
+//
+//    @Override
+//    public String toString() {
+//        return "Blog{" +
+//                "id=" + id +
+//                ", email='" + email + '\'' +
+//                ", firstname='" + firstname + '\'' +
+//                ", lastname='" + lastname + '\'' +
+//                '}';
+//    }
 }
